cd858baf3a1ea3eecdf5e07f6d4b63fd81f7a570
Dennis Schoepf <me@dnsc.io>
2021-07-24 17:21:09 +0200

    Create other sketchObjects

---
+++ src/sketchObjects/Legacy.ts
@@ -0,0 +1,23 @@
+import { mp5 } from '../../main';
+import { colors } from '../constants/colors';
+
+export class Legacy {
+  x: number;
+  y: number;
+  size: number;
+  name: string;
+  revealed: boolean;
+
+  constructor(x: number, y: number, size: number, name?: string, profileURL?: string) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  public place() {}
+
+  private draw() {
+    mp5.fill(mp5.color(colors.red));
+    mp5.ellipse(this.x, this.y, this.size);
+  }
+}
